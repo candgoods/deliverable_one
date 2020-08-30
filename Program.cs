c5fa61d87d7ecc1eb9e800fb9b4ecc958b5ef86c
Candace <goodsoncj@gmail.com>
2020-08-30 15:12:59 -0400

    first commit for deliverable 1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Deliverable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please create a passowrd with the following requirements: \n");
+            Console.WriteLine("At least one lowercase letter");
+            Console.WriteLine("At least one uppercase letter");
+            Console.WriteLine("Minimum of 7 characters");
+            Console.WriteLine("Maximum of 12 characters");
+            Console.WriteLine("An exclamation point: !\n");
+
+            string userPassword = Console.ReadLine();
+
+            //debug test below - to be removed after testing
+            //Console.WriteLine(userPassword);
+
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
